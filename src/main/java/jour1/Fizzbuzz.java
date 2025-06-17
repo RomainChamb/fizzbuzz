c870e8a5ae3e4149706a3f9f31804224455e7596
Romain <romain.code@pm.me>
2025-06-17 15:28:09 +0200

    jour1(refacto): if oneliner

--- src/main/java/jour1/Fizzbuzz.java
+++ src/main/java/jour1/Fizzbuzz.java
@@ -3,12 +3,9 @@ package jour1;
 public class Fizzbuzz {
 
     public static String fizzBuzz(int i) {
-        if(i % 15 == 0)
-            return "fizzbuzz";
-        if(i % 3 == 0)
-            return "fizz";
-        if(i % 5 == 0)
-            return "buzz";
+        if(i % 15 == 0) return "fizzbuzz";
+        if(i % 3 == 0) return "fizz";
+        if(i % 5 == 0) return "buzz";
         return String.valueOf(i);
     }
 }
