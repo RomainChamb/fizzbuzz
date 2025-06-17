8e81c35d460431effd66c69de2cdec05f0208045
Romain <romain.code@pm.me>
2025-06-17 15:24:41 +0200

    jour1(refacto): simplification if cas 3 et 5

--- src/main/java/jour1/Fizzbuzz.java
+++ src/main/java/jour1/Fizzbuzz.java
@@ -5,9 +5,9 @@ public class Fizzbuzz {
     public static String fizzBuzz(int i) {
         if(i % 15 == 0)
             return "fizzbuzz";
-        if(i == 3 || i % 3 == 0)
+        if(i % 3 == 0)
             return "fizz";
-        if(i == 5 || i % 5 == 0)
+        if(i % 5 == 0)
             return "buzz";
         return String.valueOf(i);
     }
