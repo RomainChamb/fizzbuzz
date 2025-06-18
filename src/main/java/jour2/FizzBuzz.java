a1b287b7a268440fa2e7752beebbe971a23c4fdb
Romain <romain.code@pm.me>
2025-06-18 14:38:07 +0200

    jour2(refacto): green
    Simplication du code

--- src/main/java/jour2/FizzBuzz.java
+++ src/main/java/jour2/FizzBuzz.java
@@ -2,8 +2,7 @@ package jour2;
 
 public class FizzBuzz {
     public static String fizzBuzz(int i) {
-        if(i == 2) return "2";
         if(i == 3) return "fizz";
-        return "1";
+        return String.valueOf(i);
     }
 }
