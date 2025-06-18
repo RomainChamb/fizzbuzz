f45bfbbd0c9517b3470a71ae90c4232cc28673c6
Romain <romain.code@pm.me>
2025-06-18 14:57:31 +0200

    jour2(refacto test): green
    Groupement des tests pour les valeurs sans transformation

--- src/test/java/jour2/FizzBuzzTestJour2.java
+++ src/test/java/jour2/FizzBuzzTestJour2.java
@@ -1,20 +1,24 @@
 package jour2;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTestJour2 {
 
-    @Test
-    public void doitRetourner1QuandInputEst1() {
-        assertThat(FizzBuzz.fizzBuzz(1)).isEqualTo("1");
+
+    @ParameterizedTest(name = "Quand input est {0} doit retourner {1}")
+    @CsvSource({"1,1", "2,2"})
+    @DisplayName("Doit retourner la même valeur que l'input")
+    public void doitRetourner1QuandInputEst1(int input, String expectedOutput) {
+        assertThat(FizzBuzz.fizzBuzz(input)).isEqualTo(expectedOutput);
     }
 
-    @Test
-    public void doitRetourner2QuandInputEst2() {
-        assertThat(FizzBuzz.fizzBuzz(2)).isEqualTo("2");
-    }
 
     @Test
     public void doitRetournerfizzQuandInputEst3() {
