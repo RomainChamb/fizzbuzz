efdee4b1e4207b42811740d793c2fa249c8a5e77
Romain <romain.code@pm.me>
2025-06-18 14:39:53 +0200

    jour2(test 4): green
    Input 5 doit retourner buzz

--- src/main/java/jour2/FizzBuzz.java
+++ src/main/java/jour2/FizzBuzz.java
@@ -3,6 +3,7 @@ package jour2;
 public class FizzBuzz {
     public static String fizzBuzz(int i) {
         if(i == 3) return "fizz";
+        if(i == 5) return "buzz";
         return String.valueOf(i);
     }
 }
