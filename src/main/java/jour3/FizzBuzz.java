60a7bffe5ce933385c6211973e38584941f753a8
Romain Chambonnier <ext.romain.chambonnier@sncf.fr>
2025-06-19 11:23:07 +0200

    jour3 green
    Input 1 doit retourner 1

--- src/main/java/jour3/FizzBuzz.java
+++ src/main/java/jour3/FizzBuzz.java
@@ -2,6 +2,6 @@ package jour3;
 
 public class FizzBuzz {
     public static String fizzBuzz(int i) {
-        return null;
+        return "1";
     }
 }
--- src/test/java/jour3/FizzBuzzTestJour3.java
+++ src/test/java/jour3/FizzBuzzTestJour3.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTestJour3 {
 
-    //TODO: Input 1 doit retourner "1"
     //TODO: Input 2 doit retourner "2"
     //TODO: Input 3 doit retourner "fizz"
     //TODO: Input multiple de 3 doit retourner "fizz"
