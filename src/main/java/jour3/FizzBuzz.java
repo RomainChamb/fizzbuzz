1fc0bd0ba98d214bc6908c007a02d6b152bfdf9d
Romain Chambonnier <ext.romain.chambonnier@sncf.fr>
2025-06-19 11:50:08 +0200

    jour3 refacto code
    suppression du if 15 et concaténation 3 et 5

--- src/main/java/jour3/FizzBuzz.java
+++ src/main/java/jour3/FizzBuzz.java
@@ -2,9 +2,9 @@ package jour3;
 
 public class FizzBuzz {
     public static String fizzBuzz(int i) {
-        if(i % 15 == 0) return "fizzbuzz";
-        if(i % 3 == 0) return "fizz";
-        if(i % 5 == 0) return "buzz";
-        return String.valueOf(i);
+        StringBuilder result = new StringBuilder();
+        if(i % 3 == 0) result.append("fizz");
+        if(i % 5 == 0) result.append("buzz");
+        return result.length() == 0 ? String.valueOf(i) : result.toString();
     }
 }
