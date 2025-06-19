d2cda55d88a1a70f675af6e6a9935b03cf933621
Romain Chambonnier <ext.romain.chambonnier@sncf.fr>
2025-06-19 11:30:53 +0200

    jour3 green
    Input multipe de 3 (6) doit retourner fizz

--- src/main/java/jour3/FizzBuzz.java
+++ src/main/java/jour3/FizzBuzz.java
@@ -2,7 +2,7 @@ package jour3;
 
 public class FizzBuzz {
     public static String fizzBuzz(int i) {
-        if(i == 3) return "fizz";
+        if(i % 3 == 0) return "fizz";
         return String.valueOf(i);
     }
 }
