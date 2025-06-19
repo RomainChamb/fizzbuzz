492bbbfd91ddad9cce9222f232cc65528d4a1ef5
Romain Chambonnier <ext.romain.chambonnier@sncf.fr>
2025-06-19 10:12:02 +0200

    jour3: initialisation jour 3

---
+++ src/test/java/jour3/FizzBuzzTestJour3.java
@@ -0,0 +1,13 @@
+package jour3;
+
+public class FizzBuzzTestJour3 {
+
+    //TODO: Input 1 doit retourner "1"
+    //TODO: Input 2 doit retourner "2"
+    //TODO: Input 3 doit retourner "fizz"
+    //TODO: Input multiple de 3 doit retourner "fizz"
+    //TODO: Input 5 doit retourner "buzz"
+    //TODO: Input multiple de 5 doit retourner "buzz"
+    //TODO: Input 15 doit retourner "fizzbuzz"
+    //TODO: Input multiple de quinze doit retourner "fizzbuzz"
+}
