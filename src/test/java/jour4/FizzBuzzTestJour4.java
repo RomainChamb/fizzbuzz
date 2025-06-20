42970819ac6ea50f4fb252a0684aa7fa8ec0727c
Romain <romain.code@pm.me>
2025-06-20 09:28:23 +0200

    jour4: initialisation

---
+++ src/test/java/jour4/FizzBuzzTestJour4.java
@@ -0,0 +1,21 @@
+package jour4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FizzBuzzTestJour4 {
+
+    //TODO : Input 1 doit retourner "1"
+    //TODO : Input 2 doit retourner "2"
+    //TODO : Input 3 doit retourner "fizz"
+    //TODO : Input 5 doit retourner "buzz"
+    //TODO : Input multiple 3 doit retourner "fizz"
+    //TODO : Input multiple 5 doit retourner "buzz"
+    //TODO : Input multiple 3 ET 5 doit retourner "fizzbuzz"
+
+    @Test
+    public void fizzBuzzTestJour4() {
+        assertThat(true).isTrue();
+    }
+}
