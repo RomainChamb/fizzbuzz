8b0733ab1ae6ec49082aeaf16492e56a85da3256
Romain <romain.code@pm.me>
2025-06-20 10:22:42 +0200

    jour 4 green
    Input multiple de 5 (10) doit retourner buzz

--- src/main/java/jour4/FizzBuzzJour4.java
+++ src/main/java/jour4/FizzBuzzJour4.java
@@ -3,7 +3,7 @@ package jour4;
 public class FizzBuzzJour4 {
     public static String fizzbuzz(int i) {
         if(i % 3 == 0) return "fizz";
-        if(i == 5) return "buzz";
+        if(i % 5 == 0) return "buzz";
         return String.valueOf(i);
     }
 }
