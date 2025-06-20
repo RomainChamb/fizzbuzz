322b0d32599d967782518a67c30cd1c54b4e6273
Romain <romain.code@pm.me>
2025-06-20 10:21:07 +0200

    jour 4 green
    Input multiple de 3 (6) doit retourner fizz

--- src/main/java/jour4/FizzBuzzJour4.java
+++ src/main/java/jour4/FizzBuzzJour4.java
@@ -2,7 +2,7 @@ package jour4;
 
 public class FizzBuzzJour4 {
     public static String fizzbuzz(int i) {
-        if(i == 3) return "fizz";
+        if(i % 3 == 0) return "fizz";
         if(i == 5) return "buzz";
         return String.valueOf(i);
     }
