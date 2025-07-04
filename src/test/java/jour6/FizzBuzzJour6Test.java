7edcf6b6c3cf0e65d7ad952cae91585b37800fde
Romain <romain.code@pm.me>
2025-07-04 10:08:29 +0200

    jour 6: rename test

--- src/test/java/jour6/FizzBuzzJour6Test.java
+++ src/test/java/jour6/FizzBuzzJour6Test.java
@@ -1,14 +1,13 @@
 package jour6;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzJour6Test {
 
     @Test
-    public void xxx() {
+    public void returnValueWhenIsNotMultiple() {
         assertThat("1").isEqualTo("1");
     }
 }
