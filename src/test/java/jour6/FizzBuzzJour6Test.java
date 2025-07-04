dfe7c785c8e7bdefa83fd159cd8a9aedfb34067d
Romain <romain.code@pm.me>
2025-07-04 10:21:28 +0200

    jour 6: return valuOf for 2

--- src/test/java/jour6/FizzBuzzJour6Test.java
+++ src/test/java/jour6/FizzBuzzJour6Test.java
@@ -16,7 +16,7 @@ public class FizzBuzzJour6Test {
     }
 
     private static String fizzbuzz(int i) {
-        if(i == 2 ) return "2";
+        if(i == 2 ) return String.valueOf(i);
         return "1";
     }
 }
