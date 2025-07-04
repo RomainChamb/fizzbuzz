f7dabd38cd7ffcd48a8a03729a960c6875ed9390
Romain <romain.code@pm.me>
2025-07-04 10:07:41 +0200

    jour6: premier test

---
+++ src/test/java/jour6/FizzBuzzJour6Test.java
@@ -0,0 +1,14 @@
+package jour6;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FizzBuzzJour6Test {
+
+    @Test
+    public void xxx() {
+        assertThat("1").isEqualTo("1");
+    }
+}
