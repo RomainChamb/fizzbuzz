324416b1af5d0e5f4be570cb43588b463efd3845
Romain <romain.code@pm.me>
2025-07-08 11:35:22 +0200

    jour 7: use map  for case 3

--- src/test/java/jour7/FizzBuzzJour7Test.java
+++ src/test/java/jour7/FizzBuzzJour7Test.java
@@ -39,7 +39,7 @@ public class FizzBuzzJour7Test {
     private static String fizzBuzz(int i) {
         Map<Integer, String> specialValues = specialValues();
         String result = "";
-        if(i % 3 == 0) result += "fizz";
+        if(i % 3 == 0) result += specialValues.get(3);
         if(i % 5 == 0) result += "buzz";
         return result.length() == 0 ? String.valueOf(i) : result;
     }
