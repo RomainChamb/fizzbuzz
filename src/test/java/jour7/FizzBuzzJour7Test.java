6bf3513c7d4383aba41f6fe671a924394f332f90
Romain <romain.code@pm.me>
2025-07-08 11:07:39 +0200

    jour 7: first test

--- src/test/java/jour7/FizzBuzzJour7Test.java
+++ src/test/java/jour7/FizzBuzzJour7Test.java
@@ -1,5 +1,13 @@
 package jour7;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FizzBuzzJour7Test {
 
+    @Test
+    public void test_XXX() {
+        assertThat("1").isEqualTo("1");
+    }
 }
