76a441a5b75408b825865ae79477721eeaba61c6
Romain <romain.code@pm.me>
2025-07-08 11:09:20 +0200

    jour 7: extract methode

--- src/test/java/jour7/FizzBuzzJour7Test.java
+++ src/test/java/jour7/FizzBuzzJour7Test.java
@@ -8,7 +8,11 @@ public class FizzBuzzJour7Test {
 
     @Test
     public void test_XXX() {
-        String fizzBuzz = "1";
+        String fizzBuzz = fizzBuzz();
         assertThat(fizzBuzz).isEqualTo("1");
+    }
+
+    private static String fizzBuzz() {
+        return "1";
     }
 }
