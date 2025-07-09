b6670ef7f9e075fb45e20a796567c95d2c7fc130
Romain <romain.code@pm.me>
2025-07-09 10:50:43 +0200

    jour8: green test for case 5

--- src/test/java/jour8/FizzBuzzJour8Test.java
+++ src/test/java/jour8/FizzBuzzJour8Test.java
@@ -27,6 +27,7 @@ public class FizzBuzzJour8Test {
     }
 
     private static String fizzbuzz(int i) {
+        if(i == 5) return "buzz";
         if(i % 3 == 0) return "fizz";
         return String.valueOf(i);
     }
