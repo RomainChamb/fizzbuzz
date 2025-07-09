628eebf62dbf9270330f0f8564266d23860a1ea6
Romain <romain.code@pm.me>
2025-07-09 10:37:47 +0200

    jour8: rename test

--- src/test/java/jour8/FizzBuzzJour8Test.java
+++ src/test/java/jour8/FizzBuzzJour8Test.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FizzBuzzJour8Test {
 
     @Test
-    public void testXXX() {
+    public void shouldReturnValueWhenIsNotMultiple() {
         String fizzbuzz = "1";
         assertThat(fizzbuzz).isEqualTo("1");
     }
