701396caf22dc616ca0fbbdad7b84cd43b84a6cd
Romain <romain.code@pm.me>
2025-07-09 10:42:42 +0200

    jour8: refacto

--- src/test/java/jour8/FizzBuzzJour8Test.java
+++ src/test/java/jour8/FizzBuzzJour8Test.java
@@ -20,6 +20,6 @@ public class FizzBuzzJour8Test {
 
     private static String fizzbuzz(int i) {
         if(i == 2) return String.valueOf(i);
-        return "1";
+        return String.valueOf(i);
     }
 }
