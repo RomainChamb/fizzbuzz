5eb59dea2b932577972ad7883cb34cb6b4f92558
Romain <romain.code@pm.me>
2025-07-09 10:34:24 +0200

    jour8: not compiling first test

--- src/test/java/jour8/FizzBuzzJour8Test.java
+++ src/test/java/jour8/FizzBuzzJour8Test.java
@@ -1,4 +1,11 @@
 package jour8;
 
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzJour8Test {
+
+    @Test
+    public void testXXX() {
+        assertThat("1").isEqualTo("1");
+    }
 }
