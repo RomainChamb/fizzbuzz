ac9d6239817b5874eb30eb53224aeb545667aaa8
Romain Chambonnier <ext.romain.chambonnier@sncf.fr>
2025-07-10 15:37:47 +0200

    jour 9: non compiling test 1

--- src/test/java/jour9/FizzBuzzJour9Test.java
+++ src/test/java/jour9/FizzBuzzJour9Test.java
@@ -1,4 +1,11 @@
 package jour9;
 
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzJour9Test {
+
+    @Test
+    public void testXXX() {
+        assertThat("1").isEqualTo("1");
+    }
 }
