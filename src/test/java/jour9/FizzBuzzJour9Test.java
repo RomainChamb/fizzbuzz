0bb244fca02ce27dc113c02ba6c8578a17f76399
Romain Chambonnier <ext.romain.chambonnier@sncf.fr>
2025-07-10 15:56:23 +0200

    jour 9: refacto

--- src/test/java/jour9/FizzBuzzJour9Test.java
+++ src/test/java/jour9/FizzBuzzJour9Test.java
@@ -34,7 +34,7 @@ public class FizzBuzzJour9Test {
 
 
     private static String fizzbuzz(int i) {
-        if(i % 15 == 0) return "fizzbuzz";
+        if(i % 3 == 0 && i % 5 == 0) return "fizzbuzz";
         if(i % 3 == 0) return "fizz";
         if(i % 5 == 0) return "buzz";
         return String.valueOf(i);
