36bcadcbc981e9e704897b01a5d9e973f5fc5777
Romain Chambonnier <ext.romain.chambonnier@sncf.fr>
2025-07-10 16:00:04 +0200

    jour 9: refacto introduce map of special values

--- src/test/java/jour9/FizzBuzzJour9Test.java
+++ src/test/java/jour9/FizzBuzzJour9Test.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzJour9Test {
@@ -34,6 +36,7 @@ public class FizzBuzzJour9Test {
 
 
     private static String fizzbuzz(int i) {
+        Map<Integer, String> specialValues = Map.of(3, "fizz", 5, "buzz");
         String result = "";
         if(i % 3 == 0) result += "fizz";
         if(i % 5 == 0) result += "buzz";
