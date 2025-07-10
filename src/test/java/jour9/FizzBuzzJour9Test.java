86b512ee2b639f73d363bf8453eda0859acf3e2f
Romain Chambonnier <ext.romain.chambonnier@sncf.fr>
2025-07-10 16:22:23 +0200

    jour 9: introduce loop

--- src/test/java/jour9/FizzBuzzJour9Test.java
+++ src/test/java/jour9/FizzBuzzJour9Test.java
@@ -40,8 +40,9 @@ public class FizzBuzzJour9Test {
         Map<Integer, String> specialValues = specialValues();
         Integer[] keys = specialValues.keySet().toArray(new Integer[0]);
         String result = "";
-        if(i % keys[0] == 0) result += specialValues.get(keys[0]);
-        if(i % keys[1] == 0) result += specialValues.get(keys[1]);
+        for(int key : keys) {
+            if(i % key == 0) result += specialValues.get(key);
+        }
         return result.isBlank() ? String.valueOf(i) : result;
     }
 
